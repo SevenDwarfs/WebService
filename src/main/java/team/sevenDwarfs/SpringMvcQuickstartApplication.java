a3526a507484ec16956dc5936e29a08b0e17ac8d
zhzdeng <zhzdeng@qq.com>
2017-04-09 11:10:07 +0800

    给Java文件添加注释

--- src/main/java/team/sevenDwarfs/SpringMvcQuickstartApplication.java
+++ src/main/java/team/sevenDwarfs/SpringMvcQuickstartApplication.java
@@ -1,30 +1,38 @@
 package team.sevenDwarfs;
-/**
- * 是这整个web应用的入口类，这里保持简单，仅仅设置监听端口
- */
-
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
-
+/** 
+* @ClassName: SpringMvcQuickstartApplication 
+* @Description: 是这整个web应用的入口类，这里保持简单，仅仅设置监听端口
+* @author deng
+* @date 2017年4月9日 上午11:06:07
+*/
 @SpringBootApplication
 public class SpringMvcQuickstartApplication implements EmbeddedServletContainerCustomizer {
-	/**
-	 * 整个web应用的入口点
-	 * @param args
-	 */
+	
+	/** 
+	* @Description 整个web应用的入口点
+	* @param
+	* @return void
+	* @throws
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcQuickstartApplication.class, args);
 	}
 	
+	
 	/**
-	 * 实现 EmbeddedServletContainerCustomizer 接口方法customize，这里可以设置
-	 * web应用服务器监听的端口 8080
-	 * 如果没有设置，默认监听8080端口
-	 */
+	* @Title: customize
+	* @Description: 实现 EmbeddedServletContainerCustomizer 接口方法customize，这里可以设置
+	* 				web应用服务器监听的端口 8080
+	* @param container 
+	* @see org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer#customize(
+	* 			org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer) 
+	*/
 	public void customize(ConfigurableEmbeddedServletContainer container) {
 		container.setPort(8080);
 	}
--- src/main/java/team/sevenDwarfs/controller/HelloWorldController.java
+++ src/main/java/team/sevenDwarfs/controller/HelloWorldController.java
@@ -5,21 +5,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 演示类，返回字符串helloworld，后期不用
- * @RestController 表示这一个是返回字符串的控制器
- * @EnableAutoConfiguration 使用Spring-boot的自动配置
- * @author deng
- *
- */
+/** 
+* @ClassName: HelloWorldController 
+* @Description:  演示类，返回字符串helloworld，后期不用
+* @author deng
+* @date 2017年4月9日 上午11:05:40 
+*/
 @RestController
 @EnableAutoConfiguration
 @ComponentScan(basePackages="src.main.java")
 public class HelloWorldController {
-	/**
-	 * 接受 / 下的HTTP路由
-	 * @return "Hello World" 纯文本
-	 */
+	
+	/** 
+	* @Description 接受 / 下的HTTP路由
+	* @param
+	* @return String "Hello World" 纯文本
+	* @throws
+	*/
 	@RequestMapping("/")
 	String HelloWorld() {
 		return "Hello World!";
