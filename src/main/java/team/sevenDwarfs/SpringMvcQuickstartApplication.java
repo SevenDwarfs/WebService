462a37b3d25a2fd503c93aece73c41e6649a4b95
zhzdeng <zhzdeng@qq.com>
2017-04-09 12:24:11 +0800

    加入了checkstyle代码风格检测

--- src/main/java/team/sevenDwarfs/SpringMvcQuickstartApplication.java
+++ src/main/java/team/sevenDwarfs/SpringMvcQuickstartApplication.java
@@ -1,39 +1,36 @@
-package team.sevenDwarfs;
+package team.sevendwarfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
-/** 
-* @ClassName: SpringMvcQuickstartApplication 
-* @Description: 是这整个web应用的入口类，这里保持简单，仅仅设置监听端口
+/**
+* @ClassName: SpringMvcQuickstartApplication.
+* @Description: 是这整个web应用的入口类，并以这个文件所在包为根扫描所有包下所有注解，这里保持简单，仅仅设置监听端口                   
 * @author deng
 * @date 2017年4月9日 上午11:06:07
 */
 @SpringBootApplication
 public class SpringMvcQuickstartApplication implements EmbeddedServletContainerCustomizer {
-	
-	/** 
-	* @Description 整个web应用的入口点
-	* @param
-	* @return void
-	* @throws
-	*/
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcQuickstartApplication.class, args);
-	}
-	
-	
-	/**
-	* @Title: customize
-	* @Description: 实现 EmbeddedServletContainerCustomizer 接口方法customize，这里可以设置
-	* 				web应用服务器监听的端口 8080
-	* @param container 
-	* @see org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer#customize(
-	* 			org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer) 
-	*/
-	public void customize(ConfigurableEmbeddedServletContainer container) {
-		container.setPort(8080);
-	}
+
+  /**
+   * @Description 整个web应用的入口点.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMvcQuickstartApplication.class, args);
+  }
+
+
+  /**
+  * @Title: customize
+  * @Description: 实现 EmbeddedServletContainerCustomizer 接口方法customize，这里可以设置
+  *         web应用服务器监听的端口 8080
+  * @param container 我也不知道是什么
+  * @see org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer#customize(
+  *       org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer) 
+  */
+  public void customize(ConfigurableEmbeddedServletContainer container) {
+    container.setPort(8080);
+  }
 }
--- src/main/java/team/sevenDwarfs/controller/HelloWorldController.java
+++ src/main/java/team/sevenDwarfs/controller/HelloWorldController.java
@@ -1,29 +1,25 @@
-package team.sevenDwarfs.controller;
+package team.sevendwarfs.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /** 
-* @ClassName: HelloWorldController 
+* @ClassName: HelloWorldController.
 * @Description:  演示类，返回字符串helloworld，后期不用
 * @author deng
 * @date 2017年4月9日 上午11:05:40 
 */
 @RestController
 @EnableAutoConfiguration
-@ComponentScan(basePackages="src.main.java")
 public class HelloWorldController {
-	
-	/** 
-	* @Description 接受 / 下的HTTP路由
-	* @param
-	* @return String "Hello World" 纯文本
-	* @throws
-	*/
-	@RequestMapping("/")
-	String HelloWorld() {
-		return "Hello World!";
-	}
+  
+  /** 
+  * @Description 接受 / 下的HTTP路由.
+  * @return String "Hello World" 纯文本
+  */
+  @RequestMapping("/")
+  String helloWorld() {
+    return "Hello World!";
+  }
 }
