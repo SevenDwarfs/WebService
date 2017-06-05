332357d4246344fe0673bae44eca02381218a2bd
Kinpzz <kinpzzz@gmail.com>
2017-06-05 16:34:46 +0800

    config dockerfile

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ export JAVA_HOME=/opt/java/jdk1.8.0_112
 export JENKINS_HOME=/jenkins
 
 mvn package
-docker build -t kinpzz/movie-booking .
 docker stop movie-server
 docker rm movie-server
-docker run -d -p 8082:8082 --name movie-server kinpzz/movie-booking
+docker rmi kinpzz/movie-booking
+docker build -t kinpzz/movie-booking .
+docker run -d -p 0.0.0.0:8082:8082 --name movie-server kinpzz/movie-booking
