607860d3d50cb297a5169f01e95da30a54e7ddd8
Kinpzz <kinpzzz@gmail.com>
2017-06-06 18:03:24 +0800

    config docker building process

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ export JAVA_HOME=/opt/java/jdk1.8.0_112
 export JENKINS_HOME=/jenkins
 
 mvn package
-docker stop movie-server
-docker rm movie-server
-docker rmi kinpzz/movie-booking
-docker build -t kinpzz/movie-booking .
-docker run -d -p 127.0.0.1:8082:8082 --name movie-server kinpzz/movie-booking
+docker stop RESTful-server
+docker rm RESTful-server
+docker rmi kinpzz/RESTful-server
+docker build -t kinpzz/RESTful-server .
+docker run -d -p 127.0.0.1:8082:8082 --name RESTful-server --link db:db kinpzz/RESTful-server
