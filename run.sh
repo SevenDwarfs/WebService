6dcca8018881e7281b75ef9f14d6ac9b36640110
Kinpzz <kinpzzz@gmail.com>
2017-06-06 15:29:28 +0800

    将端口隐藏，并设置nginx反向代理

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ docker stop movie-server
 docker rm movie-server
 docker rmi kinpzz/movie-booking
 docker build -t kinpzz/movie-booking .
-docker run -d -p 0.0.0.0:8082:8082 --name movie-server kinpzz/movie-booking
+docker run -d -p 127.0.0.1:8082:8082 --name movie-server kinpzz/movie-booking
