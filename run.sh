c717fb89898893674ec4d7255124fb7227e828e8
Kinpzz <kinpzzz@gmail.com>
2017-06-06 18:06:26 +0800

    config docker building process

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ export JAVA_HOME=/opt/java/jdk1.8.0_112
 export JENKINS_HOME=/jenkins
 
 mvn package
-docker stop RESTful-server
-docker rm RESTful-server
-docker rmi kinpzz/RESTful-server
-docker build -t kinpzz/RESTful-server .
-docker run -d -p 127.0.0.1:8082:8082 --name RESTful-server --link db:db kinpzz/RESTful-server
+docker stop restful-server
+docker rm restful-server
+docker rmi kinpzz/restful-server
+docker build -t kinpzz/restful-server .
+docker run -d -p 127.0.0.1:8082:8082 --name restful-server --link db:db kinpzz/restful-server
